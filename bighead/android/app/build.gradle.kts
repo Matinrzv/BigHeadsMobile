3faa0fec4f97346bce3c6193345d028ef99d05d0
Matinrzv <razavimatin36@gmail.com>
2026-02-20 14:19:01 +0330

    install app in phone

--- bighead/android/app/build.gradle.kts
+++ bighead/android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.bighead"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
